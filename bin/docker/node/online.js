97b49ed29f989a1943f921ede3e649f052c5b6f3
João Moraes <joaomoraesbr@gmail.com>
2023-06-11 17:21:24 -0700

    update

--- bin/docker/node/online.js
+++ bin/docker/node/online.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
-const { exec } = require('child_process');
-const localVersion = require('../../../package.json');
+const fs = require('fs')
 const axios = require('axios').default;
 
 let connected;
@@ -19,28 +18,40 @@ host = host
 let timeout = setTimeout(_timeout, 10000);
 let interval = setInterval(_interval, 1000);
 
+const localContent = fs.readFileSync('dist/spa/index.html').toString();
+
 function _timeout() {
   console.log('[ONLINE] Timeout');
   clearInterval(interval);
-  process.exit(1);
+  process.exit(0);
 }
 
 async function _interval() {
   console.log('[ONLINE] Try connect server', {
     host
   });
-  axios.get(`http://${host}/`)
+  axios.get(`http://${host}/`, {
+    headers: {
+      Accept: '*/*'
+    }
+  })
     .then(r => {
       const decoded = r.data;
-      console.log('[ONLINE] Incoming data', {
-        decoded
+      const local = localContent.match(/<title>(.*)<\/title>/gm);
+      const container = decoded.match(/<title>(.*)<\/title>/gm);
+      console.log({
+        local,
+        container
       })
-      if (decoded.version == localVersion.version) {
+      if (local[0] == container[0]) {
         console.log('[ONLINE] Correct version');
         connected = true;
       } else {
-        console.log('[ONLINE] Different local version of the image');
+        console.log('[ONLINE] Different local content of the image');
       }
+    })
+    .catch(e => {
+      console.error(e)
     })
   if (connected) {
     console.log('[ONLINE] Connected');
